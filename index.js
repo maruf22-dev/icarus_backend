9a2d48be269b8063a301fed41281ea4cda97cfd4
Maruf Bin Salim <marufbinsalim22@gmail.com>
2021-11-23 15:25:42 +0600

    Changed Socket Processes
    The current socket system takes all user sent message to a global thread, and then broadcasts and emits to all users in the frontend! (need to change this in future for security reasons)
    
    # good enough for beta version

--- index.js
+++ index.js
@@ -35,22 +35,6 @@ io.on('connection', (socket) => {
     console.info('socket ID: ' + socket.id + '\n');
     console.info(DividerLine);
 
-    socket.on("join_thread", (threadID) => {
-        socket.join(threadID);
-        // socket.broadcast.emit("joined_thread", { socketID : socket.id, threadID: threadID});
-        // console.info(socket.id + " joined :\n" + threadID + "\n");
-        if (threadID.includes('_')) {
-            // console.info(DividerLine);
-            // console.info('Thread is a chat between two users');
-        }
-        else {
-            // console.info(DividerLine);
-            // console.info('Thread is a group chat\n');
-            // console.info('Group ID == Thread ID\n')
-        }
-        // console.info(DividerLine);
-    });
-
     // send_to_thread
     socket.on("send_to_thread", (message) => {
         // console.log(socket.rooms);
@@ -58,8 +42,8 @@ io.on('connection', (socket) => {
         // console.info(message);
         // console.info("Thread : " + message.threadID + "\n");
         // console.info(DividerLine);
-        socket.emit("recieve_message", message);
         socket.broadcast.emit("recieve_message", message);
+        socket.emit("recieve_message", message);
         console.log(message);
     });
 
