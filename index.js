6cdb2dc5f9cdea4f61517b857503293053511fea
Maruf Bin Salim <marufbinsalim22@gmail.com>
2021-11-23 15:39:30 +0600

    removed comments and unncessary console.log

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const serverSocket = require('socket.io');
-
+// The port for the server to run on
+const PORT = process.env.SERVER_PORT || 3000;
 require('dotenv').config();
 app.use(express.json());
 app.use(cors());
-const server = require('http').createServer(app);
 
-// The port for the server to run on
-let PORT = process.env.SERVER_PORT || 3000;
 
 //requiring the routes and using them
 const API_INFO_ROUTE = require('./routes/info')
 app.use('/api/v1/info', API_INFO_ROUTE);
-
 let DividerLine = "_______________________________________\n";
 
-// socekt init
+// socekt initialization from http server
 // can be accessed from any link to handle GET and POST request
+const server = require('http').createServer(app);
 let io = serverSocket(server, {
     cors: {
         origin: "*",
@@ -28,33 +26,21 @@ let io = serverSocket(server, {
 
 // socket logic
 io.on('connection', (socket) => {
-    
-    // socket.emit(jointhread, threadID)
-    // thread_kjahsdkjashd
-    console.info('A socket connection was created\n');
-    console.info('socket ID: ' + socket.id + '\n');
-    console.info(DividerLine);
 
-    // send_to_thread
+    // send_to_thread :
+    // recieves message and sends to all users in the frontend (including the sender)
     socket.on("send_to_thread", (message) => {
-        // console.log(socket.rooms);
-        // console.info("New text socket : " + socket.id + " is :\n");
-        // console.info(message);
-        // console.info("Thread : " + message.threadID + "\n");
-        // console.info(DividerLine);
         socket.broadcast.emit("recieve_message", message);
         socket.emit("recieve_message", message);
         console.log(message);
     });
 
     socket.on("disconnect", () => {
-        // console.info("The socket " + socket.id + " was disconnected.");
-        // console.info(DividerLine);
     })
 });
 
 
-// run the server
+// start listening with the server
 server.listen(PORT, () => {
     let StartingInfo = `The Server is Listening at port : ${PORT}\n`;
     console.info(DividerLine + StartingInfo + DividerLine);
--- routes/info.js
+++ routes/info.js
@@ -3,15 +3,15 @@ let route = express.Router()
 
 // returns some basic information about the API
 route.get('/', function (req, res) {
-    let DESC_INFO = "Client-side asked for the API information.\n";
-    let API_INFO = {
+    const DividerLine = "_______________________________________\n";
+    const DESC_INFO = "Client-side asked for the API information.\n";
+    const API_INFO = {
         version: process.env.VERSION,
         author: "Md. Maruf Bin Salim",
         description: "It's the backend for CSE311 Project_1",
         initialCommit: "20/11/2021",
     }
     console.info(DESC_INFO);
-    let DividerLine = "_______________________________________\n";
     console.info(DividerLine);
     console.info(API_INFO);
     console.info(DividerLine);
